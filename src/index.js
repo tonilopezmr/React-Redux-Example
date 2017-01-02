be7019265184fc9a3c866cd7f63ccbfa46e9d596
Antonio López Marín <tonilopezmr@gmail.com>
2017-01-02 16:30:44 +0100

    Wrapped in Root component

--- src/index.js
+++ src/index.js
@@ -2,15 +2,12 @@ import React, {Component} from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 
-import {Provider} from 'react-redux'
-import TodoApp from './todo-app/components/TodoApp'
+import Root from './todo-app/components/Root'
 import configureStore from './todo-app/configureStore'
 
 const store = configureStore()
 
 ReactDOM.render(
-  <Provider store={store}>
-    <TodoApp />
-  </Provider>,
+  <Root store={store} />,
   document.getElementById('root')
 )
